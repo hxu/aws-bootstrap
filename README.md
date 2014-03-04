6135c4918cbf07348b66bbe3a0a438d5c3f7f1b2
Han Xu <hgcrpd@gmail.com>
2014-03-04 12:39:16 -0800

    fix a few bugs in setup_base

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ aws-bootstrap
 =============
 
 Setting up AWS servers for Scientific Python
+
+Overview
+========
+
+The basic workflow from scratch looks like this:
+
+ - Setup a base AMI machine image.  See `setup_base.sh`
+ - Mount a volume on ~/src, which will serve as the workspace
+ - Build one of the Docker containers in ~/src
+ - Run the Docker container, do all work in the Docker container
+
+The workspace is then persisted on the volume in the form of stopped Docker containers.
+To pick up work on an existing workspace, then you'd just do this:
+
+ - Launch a machine with the base AMI
+ - Mount the volume
+ - Restart Docker
+ - Run the Docker container/image
+
+Restarting Docker is necessary because the service is accessing the old ~/src directory, which is now
+mounted over by the volume.
--- setup_base.sh
+++ setup_base.sh
@@ -3,21 +3,23 @@
 # Ensure that python, virtualenv, and docker are installed
 
 sudo apt-get update
-sudo apt-get install python-pip git -y
+sudo apt-get install -y python-pip git
 
 # sudo pip install virtualenv virtualenvwrapper
 # Since we'll be running dockers,
 # we won't necessarily need to run virtualenvs from outside the containers
 
 # Setup docker
-sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring
+sudo apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
 # reboot
 sudo reboot
-
 curl -s https://get.docker.io/ubuntu/ | sudo sh
 
-# Modify /etc/default/docker and add the following line:
-sudo echo 'DOCKER_OPTS = "-g /home/ubuntu/src/dockers"' >> /etc/default/docker
+# Add a base mountpoint
+mkdir /home/ubuntu/src
+
+# Tell docker to use the mounted drives for docker container and image storage
+sudo /bin/sh -c 'echo DOCKER_OPTS=\"-g /home/ubuntu/src/dockers\" >> /etc/default/docker'
 # Then restart the service
 sudo service docker restart
