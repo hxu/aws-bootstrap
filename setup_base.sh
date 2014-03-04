8018cf644191f0d61cad028f89d9c639ffa4fed1
Han Xu <hgcrpd@gmail.com>
2014-03-03 17:32:49 -0800

    initial script, dockerfile

---
+++ setup_base.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/sh
+# Set up the remote base environment
+# Ensure that python, virtualenv, and docker are installed
+
+sudo apt-get update
+sudo apt-get install python-pip -y
+
+# sudo pip install virtualenv virtualenvwrapper
+# Since we'll be running dockers,
+# we won't necessarily need to run virtualenvs from outside the containers
+
+# Setup docker
+sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring
+
+# reboot
+sudo reboot
+
+curl -s https://get.docker.io/ubuntu/ | sudo sh
+
+# Modify /etc/default/docker and add the following line:
+# DOCKER_OPTS = "-g /home/ubuntu/src/dockers"
+# Then restart the service
